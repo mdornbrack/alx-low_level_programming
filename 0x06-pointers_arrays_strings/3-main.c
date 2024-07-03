e710a96a7cb8a6ab85c064a3e12528ad0ceaa707
Monique <monique.c.duplessis@gmail.com>
2024-07-03 10:42:05 -0700

    Edit files

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -1,19 +1,58 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+* main - check the code
+*
+* Return: Always 0.
+*/
 int main(void)
 {
 char s1[] = "Hello";
 char s2[] = "World";
-
-_putchar(_strcmp(s1, s2) + '0');
+int result;
+result = _strcmp(s1, s2);
+if (result < 0)
+{
+_putchar('-');
+result = -result;
+}
+else if (result == 0)
+{
+_putchar('0');
+}
+else
+{
+_putchar('+');
+}
 _putchar('\n');
-_putchar(_strcmp(s2, s1) + '0');
+result = _strcmp(s2, s1);
+if (result < 0)
+{
+_putchar('-');
+result = -result;
+}
+else if (result == 0)
+{
+_putchar('0');
+}
+else
+{
+_putchar('+');
+}
 _putchar('\n');
-_putchar(_strcmp(s1, s1) + '0');
+result = _strcmp(s1, s1);
+if (result < 0)
+{
+_putchar('-');
+result = -result;
+}
+else if (result == 0)
+{
+_putchar('0');
+}
+else
+{
+_putchar('+');
+}
 _putchar('\n');
 return (0);
 }
