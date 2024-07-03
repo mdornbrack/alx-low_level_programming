45e34cbc18ff6286a4ed592698eb7a3e11603f6b
Monique <monique.c.duplessis@gmail.com>
2024-07-03 10:35:39 -0700

    Edit file to suit Betty style and remove printf function

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -1,18 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 char s1[] = "Hello";
 char s2[] = "World";
 
-printf("%d\n", _strcmp(s1, s2));
-printf("%d\n", _strcmp(s1, s1));
-printf("%d\n", _strcmp(s2, s1));
+_putchar(_strcmp(s1, s2) + '0');
+_putchar('\n');
+_putchar(_strcmp(s2, s1) + '0');
+_putchar('\n');
+_putchar(_strcmp(s1, s1) + '0');
+_putchar('\n');
 return (0);
 }
