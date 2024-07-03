62edc853c1e5398d181f55f3950d1ccc83acbf3f
Monique <monique.c.duplessis@gmail.com>
2024-07-03 10:31:10 -0700

    Edit file to suite Betty Style and dont use printf function

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -1,11 +1,48 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * print_string - prints a string using _putchar
+ * @str: the string to print
+ */
+void print_string(char *str)
+{
+while (*str)
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
 
 /**
-* main - check the code
-*
-* Return: Always 0.
-*/
+ * print_hex - prints a string in hexadecimal format
+ * @str: the string to print
+ * @len: length of the string
+ */
+void print_hex(char *str, int len)
+{
+int i;
+
+for (i = 0; i < len; i++)
+{
+if (i % 10 == 0 && i > 0)
+{
+_putchar('\n');
+}
+else if (i % 10 != 0)
+{
+_putchar(' ');
+}
+_putchar("0123456789abcdef"[str[i] / 16]);
+_putchar("0123456789abcdef"[str[i] % 16]);
+}
+_putchar('\n');
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 char s1[98];
@@ -17,25 +54,13 @@ for (i = 0; i < 98 - 1; i++)
 s1[i] = '*';
 }
 s1[i] = '\0';
-printf("%s\n", s1);
+print_string(s1);
 ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-printf("%s\n", s1);
-printf("%s\n", ptr);
+print_string(ptr);
+print_string(s1);
 ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-printf("%s", s1);
-printf("%s", ptr);
-for (i = 0; i < 98; i++)
-{
-if (i % 10)
-{
-printf(" ");
-}
-if (!(i % 10) && i)
-{
-printf("\n");
-}
-printf("0x%02x", s1[i]);
-}
-printf("\n");
+print_string(ptr);
+print_string(s1);
+print_hex(s1, 98);
 return (0);
 }
