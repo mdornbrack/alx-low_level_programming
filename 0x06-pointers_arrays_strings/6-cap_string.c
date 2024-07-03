939442b8f6a050a44c070e30c5abe08978a97635
Monique <monique.c.duplessis@gmail.com>
2024-07-03 01:04:12 -0700

    Add files for projects

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+char *cap_string(char *str)
+{
+int i = 0;
+
+while (str[i])
+{
+if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+{
+str[i] -= 32;
+}
+if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' ||
+str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' ||
+str[i] == ')' || str[i] == '{' || str[i] == '}')
+{
+if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+{
+str[i + 1] -= 32;
+}
+}
+i++;
+}
+
+return (str);
+}
