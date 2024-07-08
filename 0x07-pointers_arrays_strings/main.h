cb224c62a63b363f0e45373eee187e53fe0275cb
Monique <monique.c.duplessis@gmail.com>
2024-07-08 00:56:53 -0700

    Task 4: Implement strpbrk function

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>  /* Include for NULL */
+
 int _putchar(char c);
 unsigned int _strspn(char *s, char *accept);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
 
 #endif /* MAIN_H */
