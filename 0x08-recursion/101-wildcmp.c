b804ceb431aacdfdde78d57554fa4ba328f457da
Monique <monique.c.duplessis@gmail.com>
2024-07-10 01:33:56 -0700

    Add wildcmp function and main file, ensure no empty spaces at the beginning of lines

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * wildcmp - Compares two strings to see if they are identical
+ * @s1: The first string to be compared
+ * @s2: The second string which can contain the special character '*'
+ *
+ * Description: The special character '*' can replace any string (including an empty string).
+ *
+ * Return: 1 if the strings are considered identical, otherwise 0
+ */
+
 int wildcmp(char *s1, char *s2)
 {
 if (*s1 == '\0' && *s2 == '\0')
