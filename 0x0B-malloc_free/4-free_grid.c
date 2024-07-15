51db1a2f2d77f794351ce782b3ac5aa5fcd3d815
Monique <monique.c.duplessis@gmail.com>
2024-07-15 00:08:55 -0700

    Add free_grid function and supporting files for freeing 2D array project

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by your alloc_grid function.
+ * free_grid - frees a 2 dimensional grid prev created by alloc_grid function.
  * @grid: The address of the two dimensional grid.
  * @height: The height of the grid.
  *
