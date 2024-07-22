ed9dbd5b43d8da101b60505a168455a33aac4217
Monique <monique.c.duplessis@gmail.com>
2024-07-21 21:59:56 -0700

    Add header file defining SIZE macro and main file to test it, including _putchar and print_number functions

---
+++ 0x0D-preprocessor/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int s;
+
+s = 98 + SIZE;
+print_number(s);
+_putchar('\n');
+return (0);
+}
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define SIZE 1024
+
+int _putchar(char c);
+void print_number(int n);
+
+#endif /* MAIN_H */
