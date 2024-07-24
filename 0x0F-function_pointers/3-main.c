178f4e00840980a7f36ae769901894d781873feb
Monique <monique.c.duplessis@gmail.com>
2024-07-24 04:02:59 -0700

    Completed calculator program with all required functions

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - check the code
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: Always 0.
+*/
+int main(int argc, char *argv[])
+{
+int num1, num2;
+int (*op_func)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+
+op_func = get_op_func(argv[2]);
+
+if (op_func == NULL || argv[2][1] != '\0')
+{
+printf("Error\n");
+return (99);
+}
+
+if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+{
+printf("Error\n");
+return (100);
+}
+
+printf("%d\n", op_func(num1, num2));
+
+return (0);
+}
