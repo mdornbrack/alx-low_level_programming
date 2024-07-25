854dff509b47af4f0a99f3108cacbe3f5739dbbd
Monique <monique.c.duplessis@gmail.com>
2024-07-25 04:19:46 -0700

    Fix Betty style issues in print_all function

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,4 +8,9 @@ int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_string(va_list args);
+
 #endif /* VARIADIC_FUNCTIONS_H */
